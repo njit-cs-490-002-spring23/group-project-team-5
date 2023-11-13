d867451e3feb44b5b16e0781a6c7692edb33297d
Norsang <thenorsang@gmail.com>
2023-11-13 18:41:52 -0500

    Updated CoveyTownSocket.d.ts

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     'prettier',
     'plugin:react-hooks/recommended',
     'plugin:react/recommended',
-    'plugin:@next/next/recommended'
+    'plugin:@next/next/recommended',
   ],
   settings: {
     react: {
