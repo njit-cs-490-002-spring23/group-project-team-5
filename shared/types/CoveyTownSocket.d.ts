f2bb07f3b60c481b777f1116f28de67cc6bcf207
Norsang Nyandak <TNorsang>
2023-11-13 17:21:05 -0500

    Commented out updated code to see what breaks the code when uncommenting each lines

--- shared/types/CoveyTownSocket.d.ts
+++ shared/types/CoveyTownSocket.d.ts
@@ -1,5 +1,3 @@
-import { Server } from "http";
-
 export type TownJoinResponse = {
   /** Unique ID that represents this player * */
   userID: string;
@@ -8,7 +6,7 @@ export type TownJoinResponse = {
   sessionToken: string;
   /** Secret token that this player should use to authenticate
    * in future requests to the video service * */
-  providerVideoToken: string;fC
+  providerVideoToken: string;
   /** List of players currently in this town * */
   currentPlayers: Player[];
   /** Friendly name of this town * */
@@ -16,15 +14,10 @@ export type TownJoinResponse = {
   /** Is this a private town? * */
   isPubliclyListed: boolean;
   /** Current state of interactables in this town */
-  interactables: TypedInteractable[];
+  interactables: Interactable[];
 }
 
-export type InteractableType = 'ConversationArea' | 'ViewingArea' | 'OneNightWerewolfArea';
-export interface Interactable {
-  type: InteractableType;
-  id: InteractableID;
-  occupants: PlayerID[];
-}
+export type Interactable = ViewingArea | ConversationArea;
 
 export type TownSettingsUpdate = {
   friendlyName?: string;
@@ -32,10 +25,8 @@ export type TownSettingsUpdate = {
 }
 
 export type Direction = 'front' | 'back' | 'left' | 'right';
-
-export type PlayerID = string;
 export interface Player {
-  id: PlayerID;
+  id: string;
   userName: string;
   location: PlayerLocation;
 };
@@ -59,8 +50,10 @@ export type ChatMessage = {
   dateCreated: Date;
 };
 
-export interface ConversationArea extends Interactable {
+export interface ConversationArea {
+  id: string;
   topic?: string;
+  occupantsByID: string[];
 };
 export interface BoundingBox {
   x: number;
@@ -69,211 +62,11 @@ export interface BoundingBox {
   height: number;
 };
 
-export interface ViewingArea extends Interactable {
+export interface ViewingArea {
+  id: string;
   video?: string;
   isPlaying: boolean;
   elapsedTimeSec: number;
-}
-
-export type GameStatus = 'IN_PROGRESS' | 'WAITING_TO_START' | 'OVER';
-/**
- * Base type for the state of a game
- */
-
-
-export interface GameState {
-  status: GameStatus;
-} 
-
-
-/**
- * Type for the state of a game that can be won
- */
-// export interface WinnableGameState extends GameState {
-//   winner?: PlayerID;
-// }
-/**
- * Base type for a move in a game. Implementers should also extend MoveType
- * @see MoveType
- */
-
-export type OneNightWerewolfArea = WinnableGameState & {
-  werewolves: Werewolf[];
-  seer: Server[];
-  robber: Robber[];
-  troublemaker: Troublemaker[];
-  villagers: Villager[];
-}
-
-export type OneNightWerewolfMove = WerewolfMove | SeerMove | RobberMove | TroublemakerMove | VillagerMove;
-
-
-export interface GameMove<MoveType> {
-  playerID: PlayerID;
-  gameID: GameInstanceID;
-  move: MoveType;
-}
-
-// Werewolf's Move
-export interface WerewolfMove extends GameMove<'WerewolfMove'> {
-  targetPlayerID: PlayerID;
-}
-
-// SeerMove's Move
-export interface SeerMove extends GameMove<'SeerMove'> {
-  targetPlayerID: PlayerID;
-}
-
-// Robber's Move
-
-export interface RobberMove extends GameMove<'RobberMove'> {
-  targetPlayerID: PlayerID;
-}
-
-// Troublemaker's Move
-export interface TroublemakerMove extends GameMove<'TroubleMakerMove'> {
-  targetPlayerID: PlayerID;
-}
-
-//  Villager's Move
-export interface VillagerMove extends GameMove<'VillagerMove'> {
-  targetPlayerID: PlayerID;
-}
-
-
-export type Werewolf = {
-  type: 'Werewolf';
-  name: string;
-};
-
-export type Seer = {
-  type: 'Seer';
-  name: string;
-}
-
-export type Robber = {
-  type: 'Robber';
-  name: string;
-}
-
-export type Troublemaker = {
-  type: 'TroubleMaker';
-  name: string;
-}
-
-export type Villager = {
-  type: 'Villager';
-  name: string;
-}
-
-
-
-// export type TicTacToeGridPosition = 0 | 1 | 2;
-
-/**
- * Type for a move in TicTacToe
- */
-// export interface TicTacToeMove {
-//   gamePiece: 'X' | 'O';
-//   row: TicTacToeGridPosition;
-//   col: TicTacToeGridPosition;
-// }
-
-/**
- * Type for the state of a TicTacToe game
- * The state of the game is represented as a list of moves, and the playerIDs of the players (x and o)
- * The first player to join the game is x, the second is o
- */
-// export interface TicTacToeGameState extends WinnableGameState {
-//   moves: ReadonlyArray<TicTacToeMove>;
-//   x?: PlayerID;
-//   o?: PlayerID;
-// }
-
-// export type InteractableID = string;
-// export type GameInstanceID = string;
-
-/**
- * Type for the result of a game
- */
-export interface GameResult {
-  gameID: GameInstanceID;
-  scores: { [playerName: string]: number };
-}
-
-/**
- * Base type for an *instance* of a game. An instance of a game
- * consists of the present state of the game (which can change over time),
- * the players in the game, and the result of the game
- * @see GameState
- */
-export interface GameInstance<T extends GameState> {
-  state: T;
-  id: GameInstanceID;
-  players: PlayerID[];
-  result?: GameResult;
-}
-
-/**
- * Base type for an area that can host a game
- * @see GameInstance
- */
-export interface GameArea<T extends GameState> extends Interactable {
-  game: GameInstance<T> | undefined;
-  history: GameResult[];
-}
-
-export type CommandID = string;
-
-/**
- * Base type for a command that can be sent to an interactable.
- * This type is used only by the client/server interface, which decorates
- * an @see InteractableCommand with a commandID and interactableID
- */
-interface InteractableCommandBase {
-  /**
-   * A unique ID for this command. This ID is used to match a command against a response
-   */
-  commandID: CommandID;
-  /**
-   * The ID of the interactable that this command is being sent to
-   */
-  interactableID: InteractableID;
-  /**
-   * The type of this command
-   */
-  type: string;
-}
-
-export type InteractableCommand =  ViewingAreaUpdateCommand | JoinGameCommand | GameMoveCommand<TicTacToeMove> | LeaveGameCommand;
-export interface ViewingAreaUpdateCommand  {
-  type: 'ViewingAreaUpdate';
-  update: ViewingArea;
-}
-export interface JoinGameCommand {
-  type: 'JoinGame';
-}
-export interface LeaveGameCommand {
-  type: 'LeaveGame';
-  gameID: GameInstanceID;
-}
-export interface GameMoveCommand<MoveType> {
-  type: 'GameMove';
-  gameID: GameInstanceID;
-  move: MoveType;
-}
-export type InteractableCommandReturnType<CommandType extends InteractableCommand> = 
-  CommandType extends JoinGameCommand ? { gameID: string}:
-  CommandType extends ViewingAreaUpdateCommand ? undefined :
-  CommandType extends GameMoveCommand<TicTacToeMove> ? undefined :
-  CommandType extends LeaveGameCommand ? undefined :
-  never;
-
-export type InteractableCommandResponse<MessageType> = {
-  commandID: CommandID;
-  interactableID: InteractableID;
-  error?: string;
-  payload?: InteractableCommandResponseMap[MessageType];
 }
 
 export interface ServerToClientEvents {
@@ -285,12 +78,305 @@ export interface ServerToClientEvents {
   townClosing: () => void;
   chatMessage: (message: ChatMessage) => void;
   interactableUpdate: (interactable: Interactable) => void;
-  commandResponse: (response: InteractableCommandResponse) => void;
 }
 
 export interface ClientToServerEvents {
   chatMessage: (message: ChatMessage) => void;
   playerMovement: (movementData: PlayerLocation) => void;
   interactableUpdate: (update: Interactable) => void;
-  interactableCommand: (command: InteractableCommand & InteractableCommandBase) => void;
 }
+// import { Server } from "http";
+
+// export type TownJoinResponse = {
+//   /** Unique ID that represents this player * */
+//   userID: string;
+//   /** Secret token that this player should use to authenticate
+//    * in future requests to this service * */
+//   sessionToken: string;
+//   /** Secret token that this player should use to authenticate
+//    * in future requests to the video service * */
+//   providerVideoToken: string;fC
+//   /** List of players currently in this town * */
+//   currentPlayers: Player[];
+//   /** Friendly name of this town * */
+//   friendlyName: string;
+//   /** Is this a private town? * */
+//   isPubliclyListed: boolean;
+//   /** Current state of interactables in this town */
+//   interactables: TypedInteractable[];
+// }
+
+// export type InteractableType = 'ConversationArea' | 'ViewingArea' | 'OneNightWerewolfArea';
+// export interface Interactable {
+//   type: InteractableType;
+//   id: InteractableID;
+//   occupants: PlayerID[];
+// }
+
+// export type TownSettingsUpdate = {
+//   friendlyName?: string;
+//   isPubliclyListed?: boolean;
+// }
+
+// export type Direction = 'front' | 'back' | 'left' | 'right';
+
+// export type PlayerID = string;
+// export interface Player {
+//   id: PlayerID;
+//   userName: string;
+//   location: PlayerLocation;
+// };
+
+// export type XY = { x: number, y: number };
+
+// export interface PlayerLocation {
+//   /* The CENTER x coordinate of this player's location */
+//   x: number;
+//   /* The CENTER y coordinate of this player's location */
+//   y: number;
+//   /** @enum {string} */
+//   rotation: Direction;
+//   moving: boolean;
+//   interactableID?: string;
+// };
+// export type ChatMessage = {
+//   author: string;
+//   sid: string;
+//   body: string;
+//   dateCreated: Date;
+// };
+
+// export interface ConversationArea extends Interactable {
+//   topic?: string;
+// };
+// export interface BoundingBox {
+//   x: number;
+//   y: number;
+//   width: number;
+//   height: number;
+// };
+
+// export interface ViewingArea extends Interactable {
+//   video?: string;
+//   isPlaying: boolean;
+//   elapsedTimeSec: number;
+// }
+
+// export type GameStatus = 'IN_PROGRESS' | 'WAITING_TO_START' | 'OVER';
+// /**
+//  * Base type for the state of a game
+//  */
+
+
+// export interface GameState {
+//   status: GameStatus;
+// } 
+
+
+// /**
+//  * Type for the state of a game that can be won
+//  */
+// // export interface WinnableGameState extends GameState {
+// //   winner?: PlayerID;
+// // }
+// /**
+//  * Base type for a move in a game. Implementers should also extend MoveType
+//  * @see MoveType
+//  */
+
+// export type OneNightWerewolfArea = WinnableGameState & {
+//   werewolves: Werewolf[];
+//   seer: Server[];
+//   robber: Robber[];
+//   troublemaker: Troublemaker[];
+//   villagers: Villager[];
+// }
+
+// export type OneNightWerewolfMove = WerewolfMove | SeerMove | RobberMove | TroublemakerMove | VillagerMove;
+
+// export interface GameMove<MoveType> {
+//   playerID: PlayerID;
+//   gameID: GameInstanceID;
+//   move: MoveType;
+// }
+
+// // Werewolf's Move
+// export interface WerewolfMove extends GameMove<'WerewolfMove'> {
+//   targetPlayerID: PlayerID;
+// }
+
+// // SeerMove's Move
+// export interface SeerMove extends GameMove<'SeerMove'> {
+//   targetPlayerID: PlayerID;
+// }
+
+// // Robber's Move
+
+// export interface RobberMove extends GameMove<'RobberMove'> {
+//   targetPlayerID: PlayerID;
+// }
+
+// // Troublemaker's Move
+// export interface TroublemakerMove extends GameMove<'TroubleMakerMove'> {
+//   targetPlayerID: PlayerID;
+// }
+
+// //  Villager's Move
+// export interface VillagerMove extends GameMove<'VillagerMove'> {
+//   targetPlayerID: PlayerID;
+// }
+
+
+// export type Werewolf = {
+//   type: 'Werewolf';
+//   name: string;
+// };
+
+// export type Seer = {
+//   type: 'Seer';
+//   name: string;
+// }
+
+// export type Robber = {
+//   type: 'Robber';
+//   name: string;
+// }
+
+// export type Troublemaker = {
+//   type: 'TroubleMaker';
+//   name: string;
+// }
+
+// export type Villager = {
+//   type: 'Villager';
+//   name: string;
+// }
+
+
+
+// // export type TicTacToeGridPosition = 0 | 1 | 2;
+
+// /**
+//  * Type for a move in TicTacToe
+//  */
+// // export interface TicTacToeMove {
+// //   gamePiece: 'X' | 'O';
+// //   row: TicTacToeGridPosition;
+// //   col: TicTacToeGridPosition;
+// // }
+
+// /**
+//  * Type for the state of a TicTacToe game
+//  * The state of the game is represented as a list of moves, and the playerIDs of the players (x and o)
+//  * The first player to join the game is x, the second is o
+//  */
+// // export interface TicTacToeGameState extends WinnableGameState {
+// //   moves: ReadonlyArray<TicTacToeMove>;
+// //   x?: PlayerID;
+// //   o?: PlayerID;
+// // }
+
+// // export type InteractableID = string;
+// // export type GameInstanceID = string;
+
+// /**
+//  * Type for the result of a game
+//  */
+// export interface GameResult {
+//   gameID: GameInstanceID;
+//   scores: { [playerName: string]: number };
+// }
+
+// /**
+//  * Base type for an *instance* of a game. An instance of a game
+//  * consists of the present state of the game (which can change over time),
+//  * the players in the game, and the result of the game
+//  * @see GameState
+//  */
+// export interface GameInstance<T extends GameState> {
+//   state: T;
+//   id: GameInstanceID;
+//   players: PlayerID[];
+//   result?: GameResult;
+// }
+
+// /**
+//  * Base type for an area that can host a game
+//  * @see GameInstance
+//  */
+// export interface GameArea<T extends GameState> extends Interactable {
+//   game: GameInstance<T> | undefined;
+//   history: GameResult[];
+// }
+
+// export type CommandID = string;
+
+// /**
+//  * Base type for a command that can be sent to an interactable.
+//  * This type is used only by the client/server interface, which decorates
+//  * an @see InteractableCommand with a commandID and interactableID
+//  */
+// interface InteractableCommandBase {
+//   /**
+//    * A unique ID for this command. This ID is used to match a command against a response
+//    */
+//   commandID: CommandID;
+//   /**
+//    * The ID of the interactable that this command is being sent to
+//    */
+//   interactableID: InteractableID;
+//   /**
+//    * The type of this command
+//    */
+//   type: string;
+// }
+
+// export type InteractableCommand =  ViewingAreaUpdateCommand | JoinGameCommand | GameMoveCommand<TicTacToeMove> | LeaveGameCommand;
+// export interface ViewingAreaUpdateCommand  {
+//   type: 'ViewingAreaUpdate';
+//   update: ViewingArea;
+// }
+// export interface JoinGameCommand {
+//   type: 'JoinGame';
+// }
+// export interface LeaveGameCommand {
+//   type: 'LeaveGame';
+//   gameID: GameInstanceID;
+// }
+// export interface GameMoveCommand<MoveType> {
+//   type: 'GameMove';
+//   gameID: GameInstanceID;
+//   move: MoveType;
+// }
+// export type InteractableCommandReturnType<CommandType extends InteractableCommand> = 
+//   CommandType extends JoinGameCommand ? { gameID: string}:
+//   CommandType extends ViewingAreaUpdateCommand ? undefined :
+//   CommandType extends GameMoveCommand<TicTacToeMove> ? undefined :
+//   CommandType extends LeaveGameCommand ? undefined :
+//   never;
+
+// export type InteractableCommandResponse<MessageType> = {
+//   commandID: CommandID;
+//   interactableID: InteractableID;
+//   error?: string;
+//   payload?: InteractableCommandResponseMap[MessageType];
+// }
+
+// export interface ServerToClientEvents {
+//   playerMoved: (movedPlayer: Player) => void;
+//   playerDisconnect: (disconnectedPlayer: Player) => void;
+//   playerJoined: (newPlayer: Player) => void;
+//   initialize: (initialData: TownJoinResponse) => void;
+//   townSettingsUpdated: (update: TownSettingsUpdate) => void;
+//   townClosing: () => void;
+//   chatMessage: (message: ChatMessage) => void;
+//   interactableUpdate: (interactable: Interactable) => void;
+//   commandResponse: (response: InteractableCommandResponse) => void;
+// }
+
+// export interface ClientToServerEvents {
+//   chatMessage: (message: ChatMessage) => void;
+//   playerMovement: (movementData: PlayerLocation) => void;
+//   interactableUpdate: (update: Interactable) => void;
+//   interactableCommand: (command: InteractableCommand & InteractableCommandBase) => void;
+// }
