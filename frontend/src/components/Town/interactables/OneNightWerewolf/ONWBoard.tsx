4ea40f64a705f35269e03963b480747c400ec937
hopeDeJesusCodes <hope.of.pa@gmail.com>
2023-12-12 23:39:44 -0500

    seer can see everyone's roles

--- frontend/src/components/Town/interactables/OneNightWerewolf/ONWBoard.tsx
+++ frontend/src/components/Town/interactables/OneNightWerewolf/ONWBoard.tsx
@@ -48,48 +48,6 @@ const RoleAssignmentScreen: React.FC<{ playerONWRole: ONWRole }> = ({ playerONWR
   </Box>
 );
 
-// eslint-disable-next-line @typescript-eslint/naming-convention
-// Custom component for the Night screen
-const NightScreen: React.FC<{
-  currentUsername: string;
-  otherPlayerUsernames: string[];
-  playerRole: ONWRole;
-}> = ({ currentUsername, otherPlayerUsernames, playerRole }) => {
-  const getNightText = () => {
-    switch (playerRole.role) {
-      case 'Villager':
-        return `${currentUsername}, pray you survive the night! One of these people are a Werewolf.`;
-      case 'Werewolf':
-        return `${currentUsername},choose who you want to kill and defend yourself in the morning.`;
-      case 'Seer':
-        return `${currentUsername},choose one of these players to reveal their role.`;
-      default:
-        return '';
-    }
-  };
-
-  return (
-    <Box textAlign='center' fontSize='xl'>
-      <Text mb={4} fontSize='2xl' fontWeight='bold'>
-        Night Time
-      </Text>
-      <Text mb={4}>{getNightText()}</Text>
-      <VStack spacing={4} align='center'>
-        {/* Use Chakra UI Button for each player */}
-        <HStack spacing={4}>
-          <VStack>
-            {otherPlayerUsernames.map(username => (
-              <Button key={username} variant='solid' colorScheme='teal'>
-                {username}
-              </Button>
-            ))}
-          </VStack>
-        </HStack>
-      </VStack>
-    </Box>
-  );
-};
-
 // Custom component for the Reveal Who Died screen
 // eslint-disable-next-line @typescript-eslint/naming-convention
 const RevealWhoDiedScreen: React.FC = () => (
@@ -157,6 +115,7 @@ export default function ONWBoard({ gameAreaController }: ONWGameProps): JSX.Elem
     .map(player => player.userName);
   const playerRole = gameAreaController.playerONWRole(townController.ourPlayer);
 
+  // Custom component for the Night screen
   const renderNightScreen = () => {
     const getNightText = () => {
       switch (playerRole.role) {
@@ -170,6 +129,33 @@ export default function ONWBoard({ gameAreaController }: ONWGameProps): JSX.Elem
           return '';
       }
     };
+    const getOtherPlayerRole = (username: string) => {
+      const otherPlayer = townController.players.find(player => player.userName === username);
+      if (otherPlayer) {
+        const otherPlayerRole = gameAreaController.playerONWRole(otherPlayer);
+        return otherPlayerRole.role;
+      }
+      return '';
+    };
+
+    const renderOtherPlayersRoles = () => {
+      if (playerRole.role === 'Seer') {
+        return (
+          <VStack spacing={4} align='center'>
+            <HStack spacing={4}>
+              <VStack>
+                {otherPlayerUsernames.map(username => (
+                  <Button key={username} variant='solid' colorScheme='teal'>
+                    {`${username}: ${getOtherPlayerRole(username)}`}
+                  </Button>
+                ))}
+              </VStack>
+            </HStack>
+          </VStack>
+        );
+      }
+      return null;
+    };
 
     return (
       <Box textAlign='center' fontSize='xl'>
@@ -177,17 +163,7 @@ export default function ONWBoard({ gameAreaController }: ONWGameProps): JSX.Elem
           Night Time
         </Text>
         <Text mb={4}>{getNightText()}</Text>
-        <VStack spacing={4} align='center'>
-          <HStack spacing={4}>
-            <VStack>
-              {otherPlayerUsernames.map(username => (
-                <Button key={username} variant='solid' colorScheme='teal'>
-                  {username}
-                </Button>
-              ))}
-            </VStack>
-          </HStack>
-        </VStack>
+        {renderOtherPlayersRoles()}
       </Box>
     );
   };
